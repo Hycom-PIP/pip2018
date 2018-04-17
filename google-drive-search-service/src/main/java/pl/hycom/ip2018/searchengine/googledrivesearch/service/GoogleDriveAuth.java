052ae74d61c5567b90ceaaf76368ac4fdd821e5c
agkan <203899@edu.p.lodz.pl>
2018-04-17 13:00:41 +0200

    usunięte niepotrzebne klasy

--- google-drive-search-service/src/main/java/pl/hycom/ip2018/searchengine/googledrivesearch/service/GoogleDriveAuth.java
+++
@@ -1,8 +0,0 @@
-package pl.hycom.ip2018.searchengine.googledrivesearch.service;
-
-import com.google.api.services.drive.Drive;
-
-public interface GoogleDriveAuth {
-
-    Drive getDriveService();
-}

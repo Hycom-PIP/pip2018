474a7cbdc6ddcf1f4aac9efbcd10a4e0e58fa6ce
Durszlak <pawel8marshall@gmail.com>
2018-04-28 02:24:29 +0200

    Hystrix & Turbine implementation on search-engine project

--- google-drive-search-service/src/main/java/pl/hycom/ip2018/searchengine/googledrivesearch/controller/GoogleDriveSearchController.java
+++ google-drive-search-service/src/main/java/pl/hycom/ip2018/searchengine/googledrivesearch/controller/GoogleDriveSearchController.java
@@ -1,6 +1,7 @@
 package pl.hycom.ip2018.searchengine.googledrivesearch.controller;
 
 import com.google.api.services.drive.Drive;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import pl.hycom.ip2018.searchengine.googledrivesearch.exception.GoogleDriveSearc
 import pl.hycom.ip2018.searchengine.googledrivesearch.service.GoogleDriveAuth;
 import pl.hycom.ip2018.searchengine.googledrivesearch.service.GoogleDriveSearch;
 import pl.hycom.ip2018.searchengine.providercontract.ProviderResponse;
+
+import java.util.ArrayList;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -29,9 +32,15 @@ public class GoogleDriveSearchController {
      * @return ProviderResponse
      * @throws GoogleDriveSearchException thrown in case of Internal Server Error
      */
+    @HystrixCommand(fallbackMethod = "getResponseFromGoogleDriveFallBack",commandKey = "fallback", groupKey = "fallback")
     @RequestMapping(value = "/res/{query}", method = GET)
     public ProviderResponse getResponseFromGoogleDrive(@PathVariable String query) throws GoogleDriveSearchException {
         Drive drive = googleDriveAuth.getAuthDriveService();
         return googleDriveSearch.getResponseFromGoogleDriveByQuery(drive, query);
     }
+
+    public ProviderResponse getResponseFromGoogleDriveFallBack(String query) {
+        return new ProviderResponse(new ArrayList<>());
+    }
+
 }
--- wikipedia-search-service/src/main/java/pl/hycom/ip2018/searchengine/wiki/WikiSearchApp.java
+++ wikipedia-search-service/src/main/java/pl/hycom/ip2018/searchengine/wiki/WikiSearchApp.java
@@ -1,8 +1,12 @@
 package pl.hycom.ip2018.searchengine.wiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 /**
@@ -11,6 +15,10 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableEurekaClient
 public class WikiSearchApp {
 
 	public static void main(String[] args) {

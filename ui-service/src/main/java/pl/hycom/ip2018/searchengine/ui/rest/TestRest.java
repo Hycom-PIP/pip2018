824c5b03da169214b8a0fc21d73efe81f8beca95
glogowski <temp96glogowski@gmail.com>
2018-05-31 11:39:41 +0200

    poprawki w html

--- ui-service/src/main/java/pl/hycom/ip2018/searchengine/ui/rest/TestRest.java
+++ ui-service/src/main/java/pl/hycom/ip2018/searchengine/ui/rest/TestRest.java
@@ -17,31 +17,28 @@ public class TestRest {
     @RequestMapping(value = "test", method = GET)
     public int test(@RequestParam("period") String period) {
         if ("7days".equals(period)) {
-            return 123;
-        } else if ("30days".equals(period)) {
             return 1234;
-        } else if ("3months".equals(period)) {
+        } else if ("30days".equals(period)) {
             return 12345;
-        } else {
+        } else if ("3months".equals(period)) {
             return 123456;
+        } else {
+            return 0;
         }
     }
 
     @RequestMapping(value = "statistics", method = GET)
     public List<TestResult> statistics(@RequestParam("period") String period) {
         if ("7days".equals(period)) {
-            return Collections.singletonList(
-                    new TestResult("xd", 100, 100f));
-        } else if ("30days".equals(period)) {
             return Arrays.asList(
                     new TestResult("tiger", 50, 50f),
                     new TestResult("t-34", 50, 50f));
-        } else if ("3months".equals(period)) {
+        } else if ("30days".equals(period)) {
             return Arrays.asList(
                     new TestResult("eliza", 33, 33.33f),
                     new TestResult("marie", 33, 33.33f),
                     new TestResult("lindsey", 33, 33.33f));
-        } else {
+        } else if ("3months".equals(period)) {
             return Arrays.asList(
                     new TestResult("studio", 6, 2.34f),
                     new TestResult("color", 5, 1.95f),
@@ -52,6 +49,8 @@ public class TestRest {
                     new TestResult("weeding", 3, 1.17f),
                     new TestResult("young", 3, 1.17f),
                     new TestResult("aerial view", 2, 0.78f));
+        } else {
+            return null;
         }
     }
 }

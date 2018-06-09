974f093b19565001c56ef6ee674f7f4b74637e1b
agkan <203899@edu.p.lodz.pl>
2018-06-09 14:48:57 +0200

    Merge remote-tracking branch 'origin/PIP-242-historia-wyszukiwania-backend' into develop
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- aggregate-service/src/main/java/pl/hycom/ip2018/searchengine/aggregate/controller/AggregateController.java
+++ aggregate-service/src/main/java/pl/hycom/ip2018/searchengine/aggregate/controller/AggregateController.java
@@ -5,37 +5,60 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import pl.hycom.ip2018.searchengine.aggregate.service.AggregateSearch;
+import pl.hycom.ip2018.searchengine.aggregate.service.CacheService;
+import pl.hycom.ip2018.searchengine.aggregate.service.CookieService;
 import pl.hycom.ip2018.searchengine.providercontract.ProviderResponse;
 
 @RestController
 @Api
+@Slf4j
 public class AggregateController {
 
     @Autowired
     private AggregateSearch aggregateSearch;
 
+    @Autowired
+    private CookieService cookieService;
+
+    @Autowired
+    private CacheService cacheService;
+
     @HystrixCommand(fallbackMethod = "getMessageFallBack", commandKey = "Aggregate-Search-Service", groupKey = "GetMessage")
     @RequestMapping(value = "/res", method = GET)
     public ProviderResponse getMessage(@RequestParam("query") String query,
-                                       @RequestParam List<String> provider) {
+            @RequestParam List<String> provider,
+            HttpServletResponse resp, HttpServletRequest req) {
+
+        cacheService.addQueryToCache(cookieService.getUserId(req, resp), query);
+
         return aggregateSearch.getResponse(query, provider);
     }
 
-    public ProviderResponse getMessageFallBack(String query, List<String> providers) {
+    public ProviderResponse getMessageFallBack(String query, List<String> provider, HttpServletResponse response, HttpServletRequest req) {
+        if (log.isWarnEnabled()) {
+            log.warn("Using fallbac for query[" + query + "]");
+        }
+
         return new ProviderResponse(new ArrayList<>());
     }
+
+    @RequestMapping(value = "/history", method = GET)
+    @ResponseBody
+    public List<String> getHistory(HttpServletResponse resp, HttpServletRequest req) {
+        return cacheService.getUserHistoryValues(cookieService.getUserId(req, resp));
+    }
 }
-
-
-//    @RequestParam("drive") String drive,
-//    @RequestParam("wikipedia") String wikipedia,
-//    @RequestParam("local") String local

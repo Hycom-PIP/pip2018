92ab4a8334bde613b2b68ebb46de30ebec372630
afoltynski <endixlol@gmail.com>
2018-06-11 23:58:19 +0200

    get query from route

--- ui-service/src/main/resources/static/js/results-vue.js
+++ ui-service/src/main/resources/static/js/results-vue.js
@@ -7,36 +7,18 @@ new Vue({
     el: '#root',
     data() {
         return {
-            parentMsg: 'Hello',
-            items: [
-                { childMsg: 'Foo' },
-                { childMsg: 'Bar' }
-            ]
+            results: null,
         }
     },
     mounted() {
-        this.fill('90')
+        this.fill()
     },
     methods: {
-        fill: function (message) {
+        fill: function () {
             // noinspection JSUnusedGlobalSymbols
             axios
-                .get('/test?period=' + message)
-                .then(response => (this.numberOfViews = response.data));
-            // noinspection JSUnusedGlobalSymbols
-            axios
-                .get('/statistics?period=' + message)
-                .then(response => (this.rows = response.data));
-            if (message === '7') {
-                // noinspection JSUnusedGlobalSymbols
-                this.timePeriod = 'Last 7 days'
-            } else if (message === '30') {
-                // noinspection JSUnusedGlobalSymbols
-                this.timePeriod = 'Last 30 days'
-            } else if (message === '90') {
-                // noinspection JSUnusedGlobalSymbols
-                this.timePeriod = 'Last 3 months'
-            }
+                .get('/query-data?query=' + $route.query.query)
+                .then(response => (this.results = response.data));
         }
     }
 });

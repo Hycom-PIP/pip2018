4c7ff2da64f7bc865b215a3600c761a967ad5f1c
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2018-06-11 22:44:33 +0200

    poprawki dla tworzenia cookie

--- aggregate-service/src/main/java/pl/hycom/ip2018/searchengine/aggregate/controller/AggregateController.java
+++ aggregate-service/src/main/java/pl/hycom/ip2018/searchengine/aggregate/controller/AggregateController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +21,6 @@ import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import pl.hycom.ip2018.searchengine.aggregate.service.AggregateSearch;
 import pl.hycom.ip2018.searchengine.aggregate.service.CacheService;
-import pl.hycom.ip2018.searchengine.aggregate.service.CookieService;
 import pl.hycom.ip2018.searchengine.providercontract.ProviderResponse;
 
 @RestController
@@ -32,18 +32,15 @@ public class AggregateController {
     private AggregateSearch aggregateSearch;
 
     @Autowired
-    private CookieService cookieService;
-
-    @Autowired
     private CacheService cacheService;
 
     @HystrixCommand(fallbackMethod = "getMessageFallBack", commandKey = "Aggregate-Search-Service", groupKey = "GetMessage")
     @RequestMapping(value = "/res", method = GET)
     public ProviderResponse getMessage(@RequestParam("query") String query,
             @RequestParam(required = false) List<String> provider,
-            HttpServletResponse resp, HttpServletRequest req) {
+            @RequestHeader("x-user-id") String userId) {
 
-        cacheService.addQueryToCache(cookieService.getUserId(req, resp), query);
+        cacheService.addQueryToCache(userId, query);
 
         return aggregateSearch.getResponse(query, provider);
     }
@@ -58,7 +55,7 @@ public class AggregateController {
 
     @RequestMapping(value = "/history", method = GET)
     @ResponseBody
-    public List<String> getHistory(HttpServletResponse resp, HttpServletRequest req) {
-        return cacheService.getUserHistoryValues(cookieService.getUserId(req, resp));
+    public List<String> getHistory(@RequestHeader("x-user-id") String userId) {
+        return cacheService.getUserHistoryValues(userId);
     }
 }

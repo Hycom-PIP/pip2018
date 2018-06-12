88f135c71381b816896e3c9489f59ff6eb77309d
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2018-06-12 02:22:45 +0200

    ga dla search async

--- ui-service/src/main/resources/static/js/results-vue.js
+++ ui-service/src/main/resources/static/js/results-vue.js
@@ -43,7 +43,7 @@ new Vue({
     },
     methods: {
     	fill: function () {
-        	var searchParams = getSearchParams();        	
+        	var searchParams = getSearchParams();   
             axios
                 .get('/query-data', {params: searchParams})
                 .then(response => (this.rows = response.data))
@@ -59,6 +59,8 @@ new Vue({
         	});
         	
         	window.history.pushState('', '', new URL(window.location.href).pathname + '?query=' + encodeURIComponent(submitEvent.target.elements.query.value) + '&provider=' + providers.join());
+        	gtag('config', GTAG_ID, {page_path: window.location.href, page_location: window.location.href});
+        	
         	axios
 	            .get('/query-data', {params: {query: submitEvent.target.elements.query.value, provider: providers}})
 	            .then(response => (this.result = response.data))
